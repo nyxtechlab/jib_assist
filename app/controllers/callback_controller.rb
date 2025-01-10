1cc68a2344db5a4454668f4ac5e1ed1d001e4cd9
PK <k.p@gmail.com>
2025-01-10 21:48:59 +0700

    fix platform

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,7 +2,7 @@ class CallbackController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def line
-    bot = Bot.find_by(name: params[:botname])
+    bot = Bot.find_by(name: params[:botname], platform: "line")
     line_client = LineClient.new(bot.line_client_token.as_json)
 
     messages = line_client.callback_to_message_object(params)
