0a6cdaf3103d23b636e1ab559f33a2cc6e3a7c4f
PK <k.p@gmail.com>
2025-01-10 21:45:52 +0700

    update code

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -3,7 +3,11 @@ class CallbackController < ApplicationController
 
   def line
     bot = Bot.find_by(name: params[:botname])
-    line_client = LineClient.new(bot.line_client_token)
+    line_client = LineClient.new(bot.line_client_token.as_json)
+
+    messages = line_client.callback_to_message_object(params)
+
+    render json: { status: "ok" }
   end
 
   def telegram
